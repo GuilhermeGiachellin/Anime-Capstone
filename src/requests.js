8d0d5e40610655b65ed6a86808666d9faa9cfad0
Anvi Alex <epononanvialex@gmail.com>
2021-08-03 17:01:21 +0300

    Display list of items on Homepage from API

---
+++ src/requests.js
@@ -0,0 +1,21 @@
+export default class request {
+
+  async get(url) {
+    const response = await fetch(url);
+    const res = await response.json();
+    return res;
+    }
+
+  async post(url, data){
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    const res = await response.json();
+    return res;
+  }
+  
+}
