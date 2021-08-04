c430b3b1f4beefcbff7f6a4cda6188d34b3654bd
Anvi Alex <epononanvialex@gmail.com>
2021-08-04 18:03:40 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
-import _ from 'lodash';
 import './style.css';
-import request from './requests';
+import Request from './requests.js';
 
-const newSession = new request();
-
+const newSession = new Request();
 
 // Get Top 9 Summer Anime From Jikan API
 newSession.get('https://api.jikan.moe/v3/season/2021/summer')
-.then(data => {
-  let container = document.querySelector('.container');
-  let domAnime = document.createElement('div');
-  domAnime.classList.add('domAnime');
-  let apiAnime = data.anime;
+  .then((data) => {
+    const container = document.querySelector('.container');
+    const domAnime = document.createElement('div');
+    domAnime.classList.add('domAnime');
+    const apiAnime = data.anime;
 
-  for(let i = 0; i < 9 ; i++){
-    let item = document.createElement('div');
-    let button = document.createElement('button');
-    let like = document.createElement('div');
-    let interaction = document.createElement('div');
-    let title = document.createElement('h5');
+    for (let i = 0; i < 9; i += 1) {
+      const item = document.createElement('div');
+      const button = document.createElement('button');
+      const like = document.createElement('div');
+      const interaction = document.createElement('div');
+      const title = document.createElement('h5');
 
-    like.classList.add('like');
-    interaction.classList.add('interaction');
-    title.classList.add('anime-title');
-    item.classList.add('item');
+      like.classList.add('like');
+      interaction.classList.add('interaction');
+      title.classList.add('anime-title');
+      item.classList.add('item');
 
-    like.innerHTML = `<i class="fas fa-heart"></i>`;
-    button.innerHTML = `Comment`;
-    title.innerHTML = `${apiAnime[i].title}`;
-    item.innerHTML += `<img src='${apiAnime[i].image_url}'/>`;
+      like.innerHTML = '<i class="fas fa-heart"></i>';
+      button.innerHTML = 'Comment';
+      title.innerHTML = `${apiAnime[i].title}`;
+      item.innerHTML += `<img src='${apiAnime[i].image_url}'/>`;
 
-    interaction.appendChild(button);
-    interaction.appendChild(like);
-    item.appendChild(title);
-    item.appendChild(interaction);
-    domAnime.appendChild(item);
-    container.appendChild(domAnime);
-  }
-})
-.catch(err => console.log(err));
+      interaction.appendChild(button);
+      interaction.appendChild(like);
+      item.appendChild(title);
+      item.appendChild(interaction);
+      domAnime.appendChild(item);
+      container.appendChild(domAnime);
+    }
+  })
+  .catch((err) => err);
--- src/requests.js
+++ src/requests.js
@@ -1,21 +1,22 @@
-export default class request {
-
+export default class Request {
   async get(url) {
+    this.url = url;
     const response = await fetch(url);
     const res = await response.json();
     return res;
-    }
+  }
 
-  async post(url, data){
+  async post(url, data) {
+    this.url = url;
+    this.data = data;
     const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
     const res = await response.json();
     return res;
   }
-  
 }
