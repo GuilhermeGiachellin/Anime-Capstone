18acf336bc5e751a370429f37c8d3373c2808042
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-08-06 12:04:16 -0300

    Linter errors

--- src/tests/countAnime.test.js
+++ src/tests/countAnime.test.js
@@ -7,7 +7,7 @@ import { countAnime /* commentCounter */ } from '../scripts/countAnime.js';
 import Request from '../scripts/requests.js';
 import displayAnime from '../scripts/displayAnime.js';
 
-test('Test Count Number function if returning exact number ',() =>{
+test('Test Count Number function if returning exact number ', () => {
   const req = new Request();
   req.get('https://api.jikan.moe/v3/season/2021/summer')
     .then((res) => {
@@ -15,7 +15,6 @@ test('Test Count Number function if returning exact number ',() =>{
     })
     .catch((err) => err);
 });
-
 
 // test('Test Count Number of comments and test if it is being printed correctly', () => {
 //   const req = new Request();
@@ -32,15 +31,12 @@ test('Test Count Number function if returning exact number ',() =>{
 test('Test Count Number of comments and test if it is being printed correctly', async () => {
   const req = new Request();
   document.body.innerHTML = '<div class="container"></div>';
-  const items;
+
   req.get('https://api.jikan.moe/v3/season/2021/summer')
     .then((res) => {
       displayAnime(res);
-      items = document.querySelectorAll('.item');
-      console.log(items);
+      const items = document.querySelectorAll('.item');
       expect(items.length).toEqual(9);
     })
     .catch((err) => err);
-  console.log(items)
 });
-
